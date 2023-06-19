dd5ac5f3d54fd6e92f92869cc424b03b08281bc4
Zubayer Ahmed Zidhan <zubaeerahmed@gmail.com>
2023-06-19 22:37:08 +0530

    Read topic names and create cards from json

--- src/app/pages/Cards.jsx
+++ src/app/pages/Cards.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import TopicCard from '../components/TopicCard';
+import { useState, useEffect } from 'react';
+
 
 
 const Cards = () => {
+
+    const [quizData, setQuizData] = useState(null);
+    const [topics, setTopics] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/quiz.json');
+                const jsonString = await response.text();
+                const jsonData = JSON.parse(jsonString);
+                setQuizData(jsonData);
+            } catch (error) {
+                console.log(error);
+            }
+            
+        }
+
+        if(quizData != null) {
+            setTopics(Object.keys(quizData));
+        } else {
+            fetchData();
+        }
+  }, [quizData]);
+
+
+    
+
+
     return (
         <div>
-            <TopicCard name="Topic Name"/>
+            {
+                topics ? topics.map((key, index) => (
+                    <TopicCard key={index} name={key.toUpperCase()} />
+                )) : <></>
+            }
         </div>
     )
 }
